d2f264403afefa04ea8c7d9bb3c6fe1656fbd839
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-06-23 13:26:06 +0700

    OK

---
+++ Struct vs Class/Area.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Struct_vs_Class
+{
+  struct Rectangle
+  {
+    int areaSize;
+    internal Rectangle(int p, int l)
+    {
+      this.areaSize = p * l;
+    }
+    internal int AreaSize { 
+      get { return areaSize; } 
+      set { areaSize = value; } 
+    }
+  }
+
+  class RectangleC
+  {
+    int areaSize;
+    internal RectangleC(int p, int l)
+    {
+      this.areaSize = p * l;
+    }
+    internal int AreaSize { 
+      get { return areaSize; } 
+      set { areaSize = value; } 
+    }
+  }
+}
--- Struct vs Class/Program.cs
+++ Struct vs Class/Program.cs
@@ -4,9 +4,30 @@ namespace Struct_vs_Class
 {
   class Program
   {
+    static void Run()
+    {
+      // struct 
+      Rectangle R = new Rectangle(2, 3);
+      Console.WriteLine(R.AreaSize);
+
+      Rectangle R2 = R;
+      R2.AreaSize = 10;
+      Console.WriteLine(R.AreaSize);
+      Console.WriteLine(R2.AreaSize);
+
+      // class
+      RectangleC RC = new RectangleC(2, 3);
+      Console.WriteLine(RC.AreaSize);
+
+      RectangleC RC2 = RC;
+      RC2.AreaSize = 10;
+      Console.WriteLine(RC.AreaSize);
+      Console.WriteLine(RC2.AreaSize);
+    }
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello Struct!");
+      Run();
     }
   }
 }
